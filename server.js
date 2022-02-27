bcc4b5ca1751977b8fcb9a55ddc06bcbb61a96ca
Sultonkul Nortoylokov <sultonkul070nortoylokov@gmail.com>
2022-02-27 14:12:37 +0500

    2.0

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const server = jsonServer.create();
 const router = jsonServer.router("./data/db.json");
 const middlewares = jsonServer.defaults();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5500;
 server.use(middlewares);
 server.use(router);
 server.listen(port, () => {
